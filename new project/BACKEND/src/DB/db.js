6dc038410318d0d72c8d0cf67149f7cb9e8e866a
Aditya Pahane <adityapahane@gmail.com>
2025-02-27 23:59:57 +0530

    new

--- new project/BACKEND/src/DB/db.js
+++ new project/BACKEND/src/DB/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
+require('dotenv').config()
 
-const connect = ()=>{mongoose.connect()
+const connect = ()=>{mongoose.connect('mongodb://0.0.0.0/practnew')
     .then(()=>{
         console.log("Connected Database")  
     })
