abba30b82584e20a5f3dcda0de54c9ef4d63f5fd
Aditya Pahane <adityapahane@gmail.com>
2025-03-01 23:11:54 +0530

    new

--- new project/BACKEND/src/Routes/routes.js
+++ new project/BACKEND/src/Routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const controller = require("../Controller/users.controller")
 
-
+router.post("/register",controller.userController)
 
 module.exports = router
--- new project/BACKEND/src/app.js
+++ new project/BACKEND/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const userRoutes = require("../src/Routes/routes")
 // const postRoutes = require("./routes/posts.routes.js")
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', userRoutes)
 
 // app.use('/users', userRoutes);
 
