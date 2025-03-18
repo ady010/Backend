0733a9dce271bca387117e01dd892e9d5a47801e
Aditya Pahane <adityapahane@gmail.com>
2025-03-18 23:52:27 +0530

    Continue...

--- messaging app/Backend/src/Routes/index.routes.js
+++ messaging app/Backend/src/Routes/index.routes.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const controller = require("../Controller/index.controller")
 
+router.post("/", controller.home)
 
 module.exports = router
