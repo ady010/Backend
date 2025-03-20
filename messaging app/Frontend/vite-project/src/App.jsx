672fb265fe2375b73fd55502b7021121e588bab7
Aditya Pahane <adityapahane@gmail.com>
2025-03-20 21:25:05 +0530

    Continue...

--- messaging app/Frontend/vite-project/src/App.jsx
+++ messaging app/Frontend/vite-project/src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <main className='bg-sky-200 h-screen w-screen '>
-      <div className="box bg-white h-[40rem] w-[50rem] border- ">
+    <main className='bg-black h-screen w-screen flex justify-center items-center'>
+      <div className="box bg-white h-[40rem] w-[60rem] rounded-4xl ">
+        <nav className='bg-amber-200 h-full w-[20rem] '>
 
+        </nav>
       </div>
     </main>
   )
